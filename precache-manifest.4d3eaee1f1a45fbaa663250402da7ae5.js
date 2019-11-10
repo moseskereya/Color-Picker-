312a239254062af7ddec66d172bb6d49d304adf6
MOSES <MOSESLAZARO2019@outlook.com>
2019-11-10 19:25:03 +0300

    Updates

---
+++ precache-manifest.4d3eaee1f1a45fbaa663250402da7ae5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7acbf72921bcce7db2d336fd20d19bd",
+    "url": "/Color-Picker-/index.html"
+  },
+  {
+    "revision": "faff3914261f0815f6d7",
+    "url": "/Color-Picker-/static/css/main.e7838246.chunk.css"
+  },
+  {
+    "revision": "0a2b5f923338d19aadcb",
+    "url": "/Color-Picker-/static/js/2.74931285.chunk.js"
+  },
+  {
+    "revision": "faff3914261f0815f6d7",
+    "url": "/Color-Picker-/static/js/main.7f66956b.chunk.js"
+  },
+  {
+    "revision": "d4821d2d269610fc5f7a",
+    "url": "/Color-Picker-/static/js/runtime-main.1d07bfb0.js"
+  }
+]);
